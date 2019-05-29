f0b6cc90344ccfadc0dd53dcb212670bb7a130cb
Kat Marchán <kzm@zkat.tech>
2019-05-29 12:50:44 -0700

    feat(api): basic implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::fs;
+use std::path::Path;
+
+use nix::unistd::{self, Uid, Gid};
+
+pub mod errors {
+    use error_chain::error_chain;
+    error_chain! {
+        foreign_links {
+            Io(::std::io::Error);
+            Nix(nix::Error);
+        }
     }
+}
+
+pub fn chownr(path: &Path, uid: Option<Uid>, gid: Option<Gid>) -> errors::Result<()> {
+    if path.is_dir() {
+        for entry in fs::read_dir(&path)? {
+            let entry = entry?;
+            chownr(entry.path().as_path(), uid, gid)?;
+        }
+    }
+    unistd::chown(path, uid, gid)?;
+    Ok(())
 }
