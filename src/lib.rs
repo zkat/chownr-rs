dfb5a66045f11da968c29efada863092afcaebd8
Kat Marchán <kzm@zkat.tech>
2019-06-02 10:41:02 +0200

    fix(errors): switch to failure
    BREAKING CHANGE: this changes the error signature

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,10 @@
 use std::fs;
 use std::path::Path;
 
+use failure::Error;
 use nix::unistd::{self, Uid, Gid};
 
-pub mod errors {
-    use error_chain::error_chain;
-    error_chain! {
-        foreign_links {
-            Io(::std::io::Error);
-            Nix(nix::Error);
-        }
-    }
-}
-
-pub fn chownr(path: &Path, uid: Option<Uid>, gid: Option<Gid>) -> errors::Result<()> {
+pub fn chownr(path: &Path, uid: Option<Uid>, gid: Option<Gid>) -> Result<(), Error> {
     if path.is_dir() {
         for entry in fs::read_dir(&path)? {
             let entry = entry?;
