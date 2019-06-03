2387f8cdda7e0938c4eb2c8c835ee2d87c7619fd
Kat Marchán <kzm@zkat.tech>
2019-06-03 23:25:58 +0200

    fix(error): use custom error pattern instead
    BREAKING CHANGE: chownr is now Result<(), chownr::Error>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
 use std::fs;
 use std::path::Path;
 
-use failure::Error;
+use failure::Fail;
 use nix::unistd::{self, Uid, Gid};
+
+#[derive(Fail, Debug)]
+pub enum Error {
+    #[fail(display = "{}", _0)]
+    Io(#[fail(cause)] std::io::Error),
+    #[fail(display = "{}", _0)]
+    Nix(#[fail(cause)] nix::Error),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(error: std::io::Error) -> Self {
+        Error::Io(error)
+    }
+}
+
+impl From<nix::Error> for Error {
+    fn from(error: nix::Error) -> Self {
+        Error::Nix(error)
+    }
+}
 
 pub fn chownr(path: &Path, uid: Option<Uid>, gid: Option<Gid>) -> Result<(), Error> {
     if path.is_dir() {
