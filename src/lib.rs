21a919e50afc906f42469f0183cc276283dd95c2
Kat Marchán <kzm@zkat.tech>
2019-10-19 22:35:09 -0400

    feat(error): switch to anyhow for error handling
    BREAKING CHANGE: This somewhat changes the returned error API in such
    a way that it's technically a breaking API change but unless you were
    handling specific errors, there shouldn't be much to change.

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,10 @@
 use std::fs;
 use std::path::Path;
 
-use failure::Fail;
-use nix::unistd::{self, Uid, Gid};
+use anyhow::Result;
+use nix::unistd::{self, Gid, Uid};
 
-#[derive(Fail, Debug)]
-pub enum Error {
-    #[fail(display = "{}", _0)]
-    Io(#[fail(cause)] std::io::Error),
-    #[fail(display = "{}", _0)]
-    Nix(#[fail(cause)] nix::Error),
-}
-
-impl From<std::io::Error> for Error {
-    fn from(error: std::io::Error) -> Self {
-        Error::Io(error)
-    }
-}
-
-impl From<nix::Error> for Error {
-    fn from(error: nix::Error) -> Self {
-        Error::Nix(error)
-    }
-}
-
-pub fn chownr(path: &Path, uid: Option<Uid>, gid: Option<Gid>) -> Result<(), Error> {
+pub fn chownr(path: &Path, uid: Option<Uid>, gid: Option<Gid>) -> Result<()> {
     if path.is_dir() {
         for entry in fs::read_dir(&path)? {
             let entry = entry?;
